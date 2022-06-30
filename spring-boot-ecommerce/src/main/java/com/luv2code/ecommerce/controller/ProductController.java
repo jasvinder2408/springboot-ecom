fbb8c2ad4f3ed28e0fab54671e3a3e6ef0d61460
Arjun <aj629280@gmail.com>
2022-06-30 16:25:20 +0530

    Requested Features are Added

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/ProductController.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.luv2code.ecommerce.controller;
 
+import com.luv2code.ecommerce.dto.ProductResponseDto;
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,8 +18,9 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-     @GetMapping("/all")
-     public ResponseEntity<List<Product>> fetchAllProduct(){
-         return ResponseEntity.ok(productService.fetchAllProduct());
-        }
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductResponseDto>> fetchAllProduct(){
+        return ResponseEntity.ok(productService.fetchAllProduct());
+
+    }
 }
--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/ProductService.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.luv2code.ecommerce.service;
 
 import com.luv2code.ecommerce.dao.ProductRepository;
+import com.luv2code.ecommerce.dto.ProductResponseDto;
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,12 +17,23 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> fetchAllProduct(){
-        List<Product> productList=productRepository.findAll();
-        for(Product product: productList){
+    /*   public List<Product> fetchAllProduct(){
+           List<Product> productList=productRepository.findAll();
 
+           return productList;
+       }
+   */
+    public List<ProductResponseDto> fetchAllProduct(){
+        List<Product> products= productRepository.findAll();
+        // List<Demo_Product> demo_products= products.stream().map(product -> new Demo_Product(product.getId(), product.getImageUrl(), product.getName())).collect(Collectors.toList());
+        ProductResponseDto demo_products=null;
+        ArrayList<ProductResponseDto> products1= new ArrayList<>();
+        for (Product product: products){
+            demo_products= new ProductResponseDto(product.getId(), product.getImageUrl(), product.getName());
+            products1.add(demo_products);
         }
-        return productList;
+        return products1;
     }
+
 
 }
